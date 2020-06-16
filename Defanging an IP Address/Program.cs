b7eede4e2c36fac739c7dbf1fef233d2edb66307
EgorSydorenko <egor.sydorenko@bmuse.com>
2020-06-16 12:39:08 +0300

    1108. Defanging an IP Address

---
+++ Defanging an IP Address/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Defanging_an_IP_Address
+{
+    class Program
+    {
+        public class Solution
+        {
+            public string DefangIPaddr(string address)
+            {
+                return address.Replace(".", "[.]");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine((new Solution().DefangIPaddr("1.1.1.1")));
+        }
+    }
+}
