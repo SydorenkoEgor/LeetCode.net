f696702e645f72a19dda56641f74f19d47c1b5ec
EgorSydorenko <egor.sydorenko@bmuse.com>
2020-06-16 12:32:09 +0300

    Shuffle the Array

---
+++ LeetCodeIssue/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeIssue
+{
+    class Program
+    {
+        static void Main()
+        {
+            var res = (new Solution().Shuffle(new int[] { 2, 5, 1, 3, 4, 7 },3));
+        }
+    }
+
+    public class Solution
+    {
+        public int[] Shuffle(int[] nums, int n)
+        {
+
+            List<int> result = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                result.Add(nums[i]);
+                result.Add(nums[n + i]);
+            }
+            return result.ToArray();
+
+        }
+    }
+}
